f740f6dbe1ec3801cd90b57438116ce97aecea30
Jana E. Beck <jana.eliz.beck@gmail.com>
2016-09-30 12:06:36 -0700

    add a note explaining what the datatypes mini-module is for

--- src/preprocessing/datatypes.js
+++ src/preprocessing/datatypes.js
@@ -15,6 +15,18 @@
  * == BSD2 LICENSE ==
  */
 
+/*
+ * This module exports the types that should be preprocessed with the Web Worker.
+ *
+ * We want to limit this to data types that we are *actually* visualizing with
+ * the processed data since we sometimes determine domain of display based on
+ * available data, with weird results when the data includes alarms that aren't
+ * visualized with timestamps in 2007 (to name an egregious example).
+ *
+ * As we add new visualizations and port tideline visualizations into viz,
+ * we will expand this list of types accordingly.
+ */
+
 export const types = [
   'cbg',
   'smbg',
