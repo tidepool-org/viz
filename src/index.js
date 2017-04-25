9b3917571520cc46c4363745e04c66b657a7be26
Jana Beck <jana.eliz.beck@gmail.com>
2017-04-24 19:11:23 -0700

    ooops addt'l merge cleanup

--- src/index.js
+++ src/index.js
@@ -31,13 +31,10 @@ import vizReducer from './redux/reducers/';
 
 import { reshapeBgClassesToBgBounds } from './utils/bloodglucose';
 
-<<<<<<< HEAD
 import createAndOpenPrintPDFPackage from './modules/print/index';
 // a temporary(?) export to enable "snapshot"-ing of data from blip console
 import { selectDailyViewData } from './modules/print/data';
 
-=======
->>>>>>> master
 const components = {
   CBGDateTraceLabel,
   FocusedRangeLabels,
