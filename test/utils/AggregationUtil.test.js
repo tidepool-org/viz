6119a49d501a0034052cdd12ad4c32249e0275f5
Clint Beacock <clint.beacock@gmail.com>
2019-09-19 09:07:34 -0400

    Fix lint errors

--- test/utils/AggregationUtil.test.js
+++ test/utils/AggregationUtil.test.js
@@ -1,11 +1,11 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
-import DataUtil from '../../src/utils/DataUtil';
-import AggregationUtil from '../../src/utils/AggregationUtil';
-import { types as Types, generateGUID } from '../../data/types';
-import { MGDL_UNITS, MS_IN_DAY, MS_IN_HOUR, MS_IN_MIN } from '../../src/utils/constants';
-/* eslint-disable max-len, no-underscore-dangle */
+// import DataUtil from '../../src/utils/DataUtil';
+// import AggregationUtil from '../../src/utils/AggregationUtil';
+// import { types as Types, generateGUID } from '../../data/types';
+// import { MGDL_UNITS, MS_IN_DAY, MS_IN_HOUR, MS_IN_MIN } from '../../src/utils/constants';
+// /* eslint-disable max-len, no-underscore-dangle */
 
 describe('AggregationUtil', () => {
-  let aggregationUtil;
+
 });
