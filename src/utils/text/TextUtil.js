8c30cf2420ed9f8f3a92d93add8c1e6f082e7b2c
Clint Beacock <clint.beacock@gmail.com>
2019-09-27 10:20:09 -0400

    Generate settings text with new TextUtil

--- src/utils/text/TextUtil.js
+++ src/utils/text/TextUtil.js
@@ -8,18 +8,22 @@ import { getPatientFullName } from '../misc';
 const t = i18next.t.bind(i18next);
 
 export class TextUtil {
-  static buildDocumentHeader = (patient) => {
+  constructor(patient) {
+    this.patient = patient;
+  }
+
+  buildDocumentHeader = () => {
     const exported = t('Exported from Tidepool: {{date}}', { date: formatCurrentDate() });
-    const bday = t('Date of birth: {{date}}', { date: formatBirthdate(patient) });
+    const bday = t('Date of birth: {{date}}', { date: formatBirthdate(this.patient) });
     const diagnosis = t('Date of diagnosis: {{date}}', { date: formatDiagnosisDate(this.patient) });
-    const fullname = getPatientFullName(patient);
+    const fullname = getPatientFullName(this.patient);
     return `${fullname}\n${bday}\n${diagnosis}\n${exported}\n`;
   };
 
-  static buildTextTable = (name, rows, columns) => `\n${name}\n${this.getTable(rows, columns)}\n`;
+  buildTextTable = (name, rows, columns) => `\n${name}\n${this.getTable(rows, columns)}\n`;
 
   getTable = (rows, columns) => {
-    const header = [this.getTableHeader(this.normalizeColumns(columns))];
+    const header = [this.getTableHeader(columns)];
     const content = this.getTableRows(rows, columns);
     return table(header.concat(content));
   };
@@ -36,7 +40,7 @@ export class TextUtil {
 
   getTableRow = (columns, rowData) => _.map(
     columns,
-    (column) => rowData[column.key]
+    column => rowData[column.key]
   );
 }
 
