e465fb40ce9adbfe4fd52dc790e3800ca369f3e8
Clint Beacock <clint.beacock@gmail.com>
2019-12-23 11:08:10 -0500

    Update aggregation util tests

--- test/utils/AggregationUtil.test.js
+++ test/utils/AggregationUtil.test.js
@@ -137,7 +137,34 @@ describe('AggregationUtil', () => {
         targetUpperBound: 180,
         targetLowerBound: 70,
         veryLowThreshold: 54,
+        clampThreshold: 600,
       });
+    });
+
+    it('should set `timezoneName` from timePrefs option', () => {
+      aggregationUtil = createAggregationUtil(data, {
+        ...defaultOpts,
+        timePrefs: { timezoneName: 'US/Eastern' },
+      });
+      expect(aggregationUtil.timezoneName).to.eql('US/Eastern');
+    });
+
+    it('should set `timezoneName` to `UTC` when not provided from timePrefs option', () => {
+      expect(aggregationUtil.timezoneName).to.eql('UTC');
+    });
+
+    it('should set `initialActiveEndpoints` as clone of dataUtil.activeEndpoints', () => {
+      expect(aggregationUtil.initialActiveEndpoints).to.eql(aggregationUtil.dataUtil.activeEndpoints);
+    });
+
+    it('should set `rangeDates` from dataUtil.activeEndpoints option in correct time zone', () => {
+      expect(aggregationUtil.rangeDates).to.eql(['2018-02-01', '2018-02-03']);
+
+      aggregationUtil = createAggregationUtil(data, {
+        ...defaultOpts,
+        timePrefs: { timezoneName: 'US/Eastern' },
+      });
+      expect(aggregationUtil.rangeDates).to.eql(['2018-01-31', '2018-02-02']);
     });
   });
 
@@ -312,6 +339,99 @@ describe('AggregationUtil', () => {
       expect(aggregationUtil.aggregateSiteChanges(groupByDate).byDate['2018-02-01'].subtotals.reservoirChange).to.equal(1);
       expect(aggregationUtil.aggregateSiteChanges(groupByDate).byDate['2018-02-02'].subtotals.tubingPrime).to.equal(1);
       expect(aggregationUtil.aggregateSiteChanges(groupByDate).byDate['2018-02-02'].subtotals.cannulaPrime).to.equal(1);
+    });
+  });
+
+  describe('aggregateDataByDate', () => {
+    let groupByDate;
+
+    beforeEach(() => {
+      aggregationUtil = createAggregationUtil(data, {
+        ...defaultOpts,
+        types: { smbg: {}, bolus: {} },
+      });
+      groupByDate = aggregationUtil.dataUtil.dimension.byDate.group();
+    });
+
+    afterEach(() => {
+      groupByDate.dispose();
+    });
+
+    it('should aggregate the data by date and by the types set in `dataUtil.types`', () => {
+      expect(aggregationUtil.aggregateDataByDate(groupByDate)['2018-02-02'].bolus).to.be.an('array').and.have.lengthOf(1);
+      expect(aggregationUtil.aggregateDataByDate(groupByDate)['2018-02-01'].bolus).to.be.an('array').and.have.lengthOf(6);
+      expect(aggregationUtil.aggregateDataByDate(groupByDate)['2018-02-02'].smbg).to.be.an('array').and.have.lengthOf(1);
+      expect(aggregationUtil.aggregateDataByDate(groupByDate)['2018-02-01'].smbg).to.be.an('array').and.have.lengthOf(3);
+    });
+
+    it('should add basals overlapping start of each date range', () => {
+      aggregationUtil = createAggregationUtil(data, {
+        ...defaultOpts,
+        types: { basal: {} },
+      });
+      groupByDate = aggregationUtil.dataUtil.dimension.byDate.group();
+
+      aggregationUtil.dataUtil.addBasalOverlappingStart = sinon.stub().callsFake(basals => {
+        basals.unshift('basal overlapping start');
+      });
+
+      expect(aggregationUtil.aggregateDataByDate(groupByDate)['2018-02-01'].basal).to.be.an('array').and.contain('basal overlapping start');
+    });
+
+    it('should reset endpoint filters to initial values after processing', () => {
+      const byEndpointsSpy = sinon.spy(aggregationUtil.dataUtil.filter, 'byEndpoints');
+      aggregationUtil.aggregateDataByDate(groupByDate);
+      sinon.assert.callCount(byEndpointsSpy, 3);
+      expect(byEndpointsSpy.getCall(2).args[0]).to.eql(aggregationUtil.initialActiveEndpoints.range);
+    });
+
+    it('should reset `dataUtil.activeEndpoints` to initial values after processing', () => {
+      aggregationUtil.aggregateDataByDate(groupByDate);
+      expect(aggregationUtil.dataUtil.activeEndpoints).to.eql(aggregationUtil.initialActiveEndpoints);
+    });
+  });
+
+  describe('aggregateStatsByDate', () => {
+    let groupByDate;
+
+    beforeEach(() => {
+      aggregationUtil = createAggregationUtil(data, {
+        ...defaultOpts,
+        stats: 'averageGlucose, timeInRange',
+      });
+      groupByDate = aggregationUtil.dataUtil.dimension.byDate.group();
+    });
+
+    afterEach(() => {
+      groupByDate.dispose();
+    });
+
+    it('should aggregate stats by date as set in `dataUtil.stats`', () => {
+      expect(aggregationUtil.aggregateStatsByDate(groupByDate)['2018-02-02'].averageGlucose).to.be.an('object').and.have.keys([
+        'total',
+        'averageGlucose',
+      ]);
+
+      expect(aggregationUtil.aggregateStatsByDate(groupByDate)['2018-02-01'].timeInRange).to.be.an('object').and.have.keys([
+        'total',
+        'veryLow',
+        'low',
+        'target',
+        'high',
+        'veryHigh',
+      ]);
+    });
+
+    it('should reset endpoint filters to initial values after processing', () => {
+      const byEndpointsSpy = sinon.spy(aggregationUtil.dataUtil.filter, 'byEndpoints');
+      aggregationUtil.aggregateStatsByDate(groupByDate);
+      sinon.assert.callCount(byEndpointsSpy, 3);
+      expect(byEndpointsSpy.getCall(2).args[0]).to.eql(aggregationUtil.initialActiveEndpoints.range);
+    });
+
+    it('should reset `dataUtil.activeEndpoints` to initial values after processing', () => {
+      aggregationUtil.aggregateStatsByDate(groupByDate);
+      expect(aggregationUtil.dataUtil.activeEndpoints).to.eql(aggregationUtil.initialActiveEndpoints);
     });
   });
 });
