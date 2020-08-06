33f01aa0173efe33df72626865f7b212476ea800
Clint Beacock <clint.beacock@gmail.com>
2020-08-06 11:49:12 -0400

    Add separate debugging timer for tagging data

--- src/utils/DataUtil.js
+++ src/utils/DataUtil.js
@@ -107,8 +107,11 @@ export class DataUtil {
     const rejectedData = _.remove(validData, d => d.reject || this.filter.byId(d.id).top(1).length);
     this.endTimer('filterValidData');
 
-    this.startTimer('addValidData');
+    this.startTimer('tagData');
     _.each(validData, this.tagDatum);
+    this.endTimer('tagData');
+
+    this.startTimer('addValidData');
     this.data.add(validData);
     this.endTimer('addValidData');
 
