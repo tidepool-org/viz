779b0cd433377aeb6deb4c167f033fd941955e9b
Clint Beacock <clint.beacock@gmail.com>
2020-09-25 15:03:22 -0400

    [WEB-981] Fix broken tests

--- test/utils/basics/data.test.js
+++ test/utils/basics/data.test.js
@@ -278,7 +278,7 @@ describe('basics data utils', () => {
     });
   });
 
-  describe('findBasicsDays', () => {
+  describe.only('findBasicsDays', () => {
     it('should always return at least 7 days, Monday thru Friday', () => {
       expect(_.map(dataUtils.findBasicsDays([
         '2015-09-07T07:00:00.000Z',
@@ -338,18 +338,18 @@ describe('basics data utils', () => {
       ]);
     });
 
-    it('should categorize each date as past, mostRecent or future', () => {
+    it('should categorize each date as `inRange` or `outOfRange`', () => {
       expect(dataUtils.findBasicsDays([
-        '2015-09-07T00:00:00.000Z',
+        '2015-09-09T00:00:00.000Z',
         '2015-09-10T12:00:00.000Z',
       ], 'Pacific/Auckland')).to.deep.equal([
-        { date: '2015-09-07', type: 'past' },
-        { date: '2015-09-08', type: 'past' },
-        { date: '2015-09-09', type: 'past' },
-        { date: '2015-09-10', type: 'mostRecent' },
-        { date: '2015-09-11', type: 'future' },
-        { date: '2015-09-12', type: 'future' },
-        { date: '2015-09-13', type: 'future' },
+        { date: '2015-09-07', type: 'outOfRange' },
+        { date: '2015-09-08', type: 'outOfRange' },
+        { date: '2015-09-09', type: 'inRange' },
+        { date: '2015-09-10', type: 'inRange' },
+        { date: '2015-09-11', type: 'outOfRange' },
+        { date: '2015-09-12', type: 'outOfRange' },
+        { date: '2015-09-13', type: 'outOfRange' },
       ]);
     });
   });
