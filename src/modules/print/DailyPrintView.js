251812c4dbe6613ae12810fe1c424f906c36575f
Clint Beacock <clint.beacock@gmail.com>
2021-01-29 12:35:11 -0500

    Revert "Allow showing total carbs stat on daily print view in exchanges"
    This reverts commit 9ed6fee6c15f6f49414a3b0b3aad5e87ec2ab4fc.

--- src/modules/print/DailyPrintView.js
+++ src/modules/print/DailyPrintView.js
@@ -590,8 +590,6 @@ class DailyPrintView extends PrintView {
         first = false;
       }
 
-      const units = _.get(this, 'latestPumpUpload.settings.units.carb') === 'exchanges' ? 'exch' : 'g';
-
       this.doc.fontSize(this.smallFontSize).font(this.boldFont)
         .text(
           t('Total Carbs'),
@@ -601,7 +599,7 @@ class DailyPrintView extends PrintView {
         )
         .font(this.font)
         .text(
-          `${formatDecimalNumber(carbs, 0)} ${units}`,
+          `${formatDecimalNumber(carbs, 0)} g`,
           { align: 'right' }
         );
     }
