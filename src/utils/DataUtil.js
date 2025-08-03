45bf8685224781905b5dbca04d868396b945e9bb
Bill Zeller <bill@tidepool.org>
2025-08-02 21:46:47 -0700

    Remove console.log() and add back in mistakenly omitted assert from test.

--- src/utils/DataUtil.js
+++ src/utils/DataUtil.js
@@ -391,7 +391,6 @@ export class DataUtil {
         d.dosingDecision.pumpSettings = this.pumpSettingsDatumsByIdMap[associatedPumpSettingsId];
 
         // Translate relevant dosing decision data onto expected bolus fields
-        console.log(d.dosingDecision, 'dosing decision')
         d.carbInput = d.dosingDecision.originalFood?.nutrition?.carbohydrate?.net ??
               d.dosingDecision.food?.nutrition?.carbohydrate?.net; // use originalFood if present, as this is the original value present at time of bolus
         d.bgInput = d?.dosingDecision?.smbg?.value || _.last(d.dosingDecision.bgHistorical || [])?.value;
