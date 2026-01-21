0ec041c7efb8e27558a01caee0332c789b05e8ba
clintonium-119 <clint.beacock@gmail.com>
2026-01-21 16:28:12 -0500

    Add dataUtil.addLocalData method to allow adding locally downloaded data sets

--- storybook/preview.js
+++ storybook/preview.js
@@ -2,28 +2,9 @@ import React from 'react';
 
 import DataUtil from '../src/utils/DataUtil';
 
-let data;
-try {
-  // eslint-disable-next-line global-require, import/no-unresolved
-  data = require('../local/rawData.json');
-  let dataSource = 'the Tidepool API';
-
-  if (data?.data?.current?.data) {
-    data = _.flatten(_.values(data.data.current.data));
-    dataSource = 'a Tidepool Web console export';
-  } else if (data?.[0].dataset) {
-    data = _.flatten(_.map(data, v => v.data));
-    dataSource = 'a Tidepool Account Tool export';
-  }
-
-  console.log(`Loading dataset provided by ${dataSource}`);
-} catch (e) {
-  data = { data: [] };
-}
-
 const patientId = 'abc123';
 const dataUtil = new DataUtil();
-dataUtil.addData(data, patientId);
+dataUtil.addLocalData(patientId);
 
 const props = {
   dataUtil,
