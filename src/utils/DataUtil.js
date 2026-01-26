327428da48e123d86b18f68c2752f1b1c1f93fc5
henry-tp <henry.liang@tidepool.org>
2026-01-26 14:47:20 -0800

    WEB-4330 update tests

--- src/utils/DataUtil.js
+++ src/utils/DataUtil.js
@@ -1483,11 +1483,11 @@ export class DataUtil {
         // Clock drift on user's device may cause pumpSettings datum to have LATER timestamp than upload
         // datum. The maximum time devation that Uploader allows between user device and tidepool server
         // time is 15 minutes, so we search 15 minutes into the future for the pumpSettings datum
-        const TIME_DEVIATION_TOLERANCE = 15 * MS_IN_MIN;
+        const UPLOADER_TIME_DEVIATION_TOLERANCE = 15 * MS_IN_MIN;
 
         pumpSettingsForUpload = _.filter(
           pumpSettingsForUpload,
-          ps => ps.time <= (latestPumpUpload.time + TIME_DEVIATION_TOLERANCE)
+          ps => ps.time <= (latestPumpUpload.time + UPLOADER_TIME_DEVIATION_TOLERANCE)
         );
       }
 
