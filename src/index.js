0005fb15d47e2b609351f2186d0786242fb8b1bb
henry-tp <henry.liang@tidepool.org>
2026-09-22 14:52:28 -0700

    WEB-4740 export formatChartDateBounds fn

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ import {
 import colors from './colors';
 
 import {
+  formatChartDateBounds,
   formatDateRange,
   formatTimeAgo,
   getLocalizedCeiling,
@@ -155,6 +156,7 @@ const utils = {
   },
   datetime: {
     findBasicsStart,
+    formatChartDateBounds,
     formatDateRange,
     formatTimeAgo,
     getLocalizedCeiling,
